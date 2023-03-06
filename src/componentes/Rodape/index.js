c59dac89920eb65c4318c305dba09f0864168399
Tayná Mayer <112437100+TaynaMayer@users.noreply.github.com>
2023-03-06 15:30:28 -0300

    Projeto completo

---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,32 @@
+import './Rodape.css'
+
+const Rodape = () => {
+    return (<footer className="footer">        
+        <section>
+            <ul>
+                <li>
+                    <a href="facebook.com" target="_blank">
+                        <img src="/imagens/fb.png" alt="" />
+                    </a>
+                </li>
+                <li>
+                    <a href="twitter.com" target="_blank">
+                        <img src="/imagens/tw.png" alt="" />
+                    </a>
+                </li>
+                <li>
+                    <a href="instagram.com" target="_blank">
+                        <img src="/imagens/ig.png" alt="" />
+                    </a>
+                </li>
+            </ul>
+        </section>
+        <section>
+            <a href="logo.com" target="_blank">
+                <img src="/imagens/logo.png" alt="" />
+            </a>
+        </section>        
+    </footer>)
+}
+
+export default Rodape
